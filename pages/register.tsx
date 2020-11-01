f86f73ec867fd11137ac3543ce74094ca8812f9d
Panos61 <K702943@students.kastoria.teiwm.gr>
2020-11-01 19:52:03 +0200

    +style

--- pages/register.tsx
+++ pages/register.tsx
@@ -8,7 +8,10 @@ import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
+import Link from 'next/link';
+import Head from 'next/head';
+import Fab from '@material-ui/core/Fab';
+//import { Formik, Form } from 'formik';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,19 +28,54 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const globalStyle = `
+body {
+  background-color: #491e07;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%23cc0000' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23aa0000' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23d6002b' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23b10022' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d9004b' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23b2003d' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23d3006c' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23ac0057' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23c4008c' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%239e0071' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23aa00aa' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23880088' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
+}
+`;
+
 const Register = () => {
   const classes = useStyles();
 
+  //const register;
+
   return (
     <>
+      <Head>
+        <style>{globalStyle}</style>
+      </Head>
       <CssBaseline />
-      <Box mt={15}>
+
+      <Box mt={5}>
+        <Fab
+          style={{ textTransform: 'none' }}
+          variant='extended'
+          size='small'
+          color='secondary'
+        >
+          <Link href='/'>
+            <span style={{ textDecoration: 'none' }}>Αρχική</span>
+          </Link>
+        </Fab>
+      </Box>
+      <Box mt={10}>
         <Container maxWidth='sm'>
           <Card className={classes.root} elevation={5}>
             <CardContent>
-              <h2 style={{ fontFamily: 'sans-serif', color: 'ff9800' }}>
-                Register
+              <h2 style={{ fontFamily: 'sans-serif', color: 'black' }}>
+                Εγγραφή στο <span style={{ color: ' #FF6F1D' }}>eventhub_</span>
               </h2>
+              {/* <Formik 
+              initialValues={{email: '', username: '', password: '', confirmPass: ''}}
+              onSubmit={async (values, {setErrors}) => {
+                const response = await register({
+                  variables: {options: values},
+                  update: (cachem)
+                })
+              }} */}
               <form className={classes.form} noValidate>
                 <Grid container spacing={2}>
                   <Grid item xs={12}>
@@ -106,9 +144,7 @@ const Register = () => {
                   style={{ textAlign: 'center' }}
                 >
                   <Grid item xs={12}>
-                    <Link href='#' variant='body2'>
-                      Έχετε ήδη λογαριασμό; Συνδεθείτε!
-                    </Link>
+                    <Link href='/login'>Έχετε ήδη λογαριασμό; Συνδεθείτε!</Link>
                   </Grid>
                 </Grid>
               </form>
