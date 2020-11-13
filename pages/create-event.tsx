5b0be5a64f4bacb3b2ac05bb83c929bd4366ca5a
Panos61 <panagiwtis.orovas@gmail.com>
2020-11-13 12:27:47 +0200

    Options Page

--- pages/create-event.tsx
+++ pages/create-event.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-//import Head from 'next/head';
+import Head from 'next/head';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField/TextField';
 import { isAuth } from '../src/utils/isAuth';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Link from 'next/link';
 
 interface createProps {}
 
-// const globalStyle = `
-// body {
-//   background-color: white;
-//   background-size: cover;
-// }
-// `;
+const globalStyle = `
+body {
+  background-color: #061220;
+  background-size: cover;
+}
+`;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,7 +34,7 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       fontWeight: 'bold',
       textAlign: 'center',
-      color: '#4caf50',
+      color: '#d32f2f',
     },
     paper: {
       minHeight: '70vh',
@@ -45,6 +47,10 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: theme.spacing(4),
       color: theme.palette.error.main,
     },
+    button: {
+      textTransform: 'none',
+      fontSize: '18px',
+    },
     submit: {
       margin: theme.spacing(3, 0, 3),
     },
@@ -56,25 +62,37 @@ const CreateEvent: React.FC<createProps> = () => {
 
   const classes = useStyles();
   return (
-    // <Head>
-    //     <style>{globalStyle}</style>
-    //   </Head>
-
     <div className={classes.root}>
+      <Head>
+        <style>{globalStyle}</style>
+      </Head>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <h2 className={classes.title}>Δημιουργία Event</h2>
           <Paper elevation={3} className={classes.paper}>
-            <Grid item xs={12} className={classes.formRoot}>
-              <TextField
-                id='email'
-                label='Email'
-                name='email'
-                autoComplete='email'
-                fullWidth
-                variant='outlined'
-              />
-            </Grid>
+            <Box mt={20}>
+              <Grid item xs={12} className={classes.formRoot}>
+                <Link href='create-single-event'>
+                  <Button
+                    variant='contained'
+                    className={classes.button}
+                    style={{ backgroundColor: '#43a047' }}
+                  >
+                    Μονό
+                  </Button>
+                </Link>
+              </Grid>
+              <Grid item xs={12} className={classes.formRoot}>
+                <Button
+                  variant='contained'
+                  className={classes.button}
+                  style={{ backgroundColor: '#fdd835' }}
+                  disabled
+                >
+                  Festival
+                </Button>
+              </Grid>
+            </Box>
           </Paper>
         </Grid>
       </Grid>
